9f00510d109d9723eeabab7e3c67afa12deaddc2
AbidAngkon <abidangkon@gmail.com>
2024-02-12 00:31:15 +0600

    angkon

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// var htmlContent = `<div style="background: green; height: 200px; width: 200px">
+//             <ul>
+//                 <li>Item 1</li>
+//                 <li>Item 2</li>
+//                 <li>Item 3</li>
+//                 <li>Item 4</li>
+//             </ul>
+//         </div>`;
+
+//         // Get the element by ID
+//         var demoElement = document.getElementById('demo');
+
+//         // Set the innerHTML of the demo element to the HTML content
+//         demoElement.innerHTML = htmlContent;
